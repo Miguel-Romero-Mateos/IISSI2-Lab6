96931e38abacb6e84d9bb170067621a659950549
MIGUEL ROMERO <migrommat@alum.us.es>
2026-05-12 21:05:06 +0200

    Lab6 - Completado

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateProductScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateProductScreen.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Pressable, ScrollView, StyleSheet, Switch, View } from 'react-native'
+import { Image, Platform, Pressable, ScrollView, StyleSheet, Switch, View } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import InputItem from '../../components/InputItem'
 import TextRegular from '../../components/TextRegular'
@@ -13,5 +13,104 @@ import { ErrorMessage, Formik } from 'formik'
 import TextError from '../../components/TextError'
 import ImagePicker from '../../components/ImagePicker'
 
-export default function CreateProductScreen({ navigation, route }) {
+
+
+export default function CreateProductScreen({ navigation }) {
+    const initialProductValues = {
+        name: null,
+        description: null,
+        price: null,
+        image: null,
+        productCategory: null,
+        availability: null,
+    }
+    return (
+        <Formik
+        initialValues={initialProductValues}
+        >
+        {({ setFieldValue, values }) => (
+            <ScrollView>
+            <View style={{ alignItems: 'center' }}>
+                <View style={{ width: '60%' }}>
+                <ImagePicker
+                    name='image'
+
+                    label='Image:'
+                    image={values.image}
+                    onImagePicked={result => setFieldValue('image', result)}
+                />
+                <InputItem
+                    name='name'
+                    label='Name:'
+                />
+                <InputItem
+                    name='description'
+                    label='Description:'
+                />
+                <InputItem
+                    name='price'
+                    label='Price:'
+                />
+                <InputItem
+                    name='productCategory'
+                    label='Product Category:'
+                />
+                <InputItem
+                    name='availability'
+                    label='Availability:'
+                />
+                <TextRegular>Is it available?</TextRegular>
+                <Switch
+                trackColor={{
+                    false: GlobalStyles.brandSecondary,
+                    true: GlobalStyles.brandPrimary
+                }}
+                thumbColor={
+                    values.availability ? GlobalStyles.brandSecondary : '#f4f3f4'
+                }
+                value={values.availability}
+                style={styles.switch}
+                onValueChange={value => setFieldValue('availability', value)}   // si la función es más compleja, podemos invocar otra función así onValueChange={toggleSwitch}
+
+                />
+                <Pressable
+                    onPress={() => console.log('Button pressed')}
+                    style={({ pressed }) => [
+                        {
+                            backgroundColor: pressed
+                            ? GlobalStyles.brandPrimaryTap
+                            : GlobalStyles.brandPrimary
+                        },
+                        styles.button
+                        ]}>
+                        <TextRegular textStyle={styles.text}>
+                            Create product
+                        </TextRegular>
+                </Pressable>
+                </View>
+            </View>
+            </ScrollView>
+        )}
+        </Formik>
+  )
 }
+
+const styles = StyleSheet.create({
+    button: {
+        borderRadius: 8,
+        height: 40,
+        padding: 10,
+        width: '100%',
+        marginTop: 20,
+        marginBottom: 20
+    },
+    text: {
+        fontSize: 16,
+        color: 'white',
+        textAlign: 'center',
+        marginLeft: 5
+    },
+    switch: {
+        marginTop: 20,
+    }
+})
--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react'
-import { Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import { Pressable, ScrollView, StyleSheet, Switch, View } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 import * as yup from 'yup'
 import DropDownPicker from 'react-native-dropdown-picker'
@@ -14,8 +14,143 @@ import { ErrorMessage, Formik } from 'formik'
 import TextError from '../../components/TextError'
 import ImagePicker from '../../components/ImagePicker'
 
+
 export default function CreateRestaurantScreen({ navigation }) {
 
+    const initialRestaurantValues = {
+        name: null,
+        description: null,
+        address: null,
+        postalCode: null,
+        url: null,
+        shippingCosts: null,
+        email: null,
+        phone: null,
+        restaurantCategoryId: null
+    }
+
+    const [restaurantCategories, setRestaurantCategories] = useState([])
+    const [open, setOpen] = useState(false)
+    useEffect(() => {
+        async function fetchRestaurantCategories() {
+            try {
+                const fetchedRestaurantCategories = await getRestaurantCategories()
+                const fetchedRestaurantCategoriesReshaped =
+                fetchedRestaurantCategories.map(e => {
+                    return {
+                    label: e.name,
+                    value: e.id
+                    }
+                })
+                setRestaurantCategories(fetchedRestaurantCategoriesReshaped)
+            } catch (error) {
+                showMessage({
+                message: `There was an error while retrieving restaurant categories. ${error} `,
+                type: 'error',
+                style: GlobalStyles.flashStyle,
+                titleStyle: GlobalStyles.flashTextStyle
+                })
+            }
+        }
+        fetchRestaurantCategories()
+    }, [])
 
 
+    return (
+        <Formik
+        initialValues={initialRestaurantValues}
+        >
+        {({ setFieldValue, values }) => (
+            <ScrollView>
+            <View style={{ alignItems: 'center' }}>
+                <View style={{ width: '60%' }}>
+                <ImagePicker
+                    name='logo'
+                    label='Logo:'
+                    image={values.logo}
+                    onImagePicked={result => setFieldValue('logo', result)}
+                />
+                <InputItem
+                    name='name'
+                    label='Name:'
+                />
+                <InputItem
+                    name='description'
+                    label='Description:'
+                />
+                <InputItem
+                    name='address'
+                    label='Address:'
+                />
+                <InputItem
+                    name='postalCode'
+                    label='Postal Code:'
+                />
+                <InputItem
+                    name='url'
+                    label='URL:'
+                />
+                <InputItem
+                    name='shippingCosts'
+                    label='Shipping Costs:'
+                />
+                <InputItem
+                    name='email'
+                    label='Email:'
+                />
+                <InputItem
+                    name='phone'
+                    label='Phone:'
+                />
+                <DropDownPicker
+                    open={open}
+                    value={values.restaurantCategoryId}
+                    items={restaurantCategories}
+                    setOpen={setOpen}
+                    onSelectItem={item => {
+                        setFieldValue('restaurantCategoryId', item.value)
+                    }}
+                    setItems={setRestaurantCategories}
+                    placeholder="Select the restaurant category"
+                    containerStyle={{ height: 40, marginTop: 20 }}
+                    style={{ backgroundColor: GlobalStyles.brandBackground }}
+                    dropDownStyle={{ backgroundColor: '#fafafa' }}
+                />
+                <Pressable
+                    onPress={() => console.log('Button pressed')}
+                    style={({ pressed }) => [
+                        {
+                            backgroundColor: pressed
+                            ? GlobalStyles.brandPrimaryTap
+                            : GlobalStyles.brandPrimary
+                        },
+                        styles.button
+                        ]}>
+                        <TextRegular textStyle={styles.text}>
+                            Create restaurant
+                        </TextRegular>
+                </Pressable>
+                </View>
+            </View>
+            </ScrollView>
+        )}
+        </Formik>
+  )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginLeft: 5
+  }
+})
